7ea0f586f83a6f7349c3bdfeb40d8d46677aa15d
ThomasEll <longdotw@hotmail.co.uk>
2015-12-06 18:03:36 +0000

    Removed unnecessary imports

--- src/com/bulenkov/game2048/Game2048.java
+++ src/com/bulenkov/game2048/Game2048.java
@@ -20,9 +20,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 /**
